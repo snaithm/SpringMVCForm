14ca2aff9f85996e8a783cb6ecd63ef7b22cc2cc
Matthew Snaith <mathswan1984@gmail.com>
2017-02-28 08:54:15 +0000

    model changes

---
+++ src/main/java/com/hello/Greeting.java
@@ -0,0 +1,24 @@
+package com.hello;
+
+public class Greeting {
+
+    private String name;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
